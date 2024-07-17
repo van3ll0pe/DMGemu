e3deefb1e46fe38dddc1999f008974b25dce979c
van3ll0pe <van3ll0pe.chell@gmail.com>
2024-07-17 21:53:21 +0200

    FINISH CPU FIX: memory init with HARD REGISTER

--- src/cpu.c
+++ src/cpu.c
@@ -1961,102 +1961,198 @@ void cpu_execute_instruction_CB(Cpu* cpu)
                     break;
 
         case 0xD0:  //SET 2, B
+                    cpu_instr_SET(cpu, BIT_2, &cpu->BC.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xD1:  //SET 2, C
+                    cpu_instr_SET(cpu, BIT_2, &cpu->BC.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xD2:  //SET 2, D
+                    cpu_instr_SET(cpu, BIT_2, &cpu->DE.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xD3:  //SET 2, E
+                    cpu_instr_SET(cpu, BIT_2, &cpu->DE.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xD4:  //SET 2, H
+                    cpu_instr_SET(cpu, BIT_2, &cpu->HL.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xD5:  //SET 2, L
+                    cpu_instr_SET(cpu, BIT_2, &cpu->HL.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xD6:  //SET 2, [HL]
+                    cpu_instr_SETHL(cpu, BIT_2, cpu->HL.r16);
+                    cpu->cycle = 16;
                     break;
         case 0xD7:  //SET 2, A
+                    cpu_instr_SET(cpu, BIT_2, &cpu->AF.r8.hi);
+                    cpu->cycle  = 8;
                     break;
         case 0xD8:  //SET 3, B
+                    cpu_instr_SET(cpu, BIT_3, &cpu->BC.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xD9:  //SET 3, C
+                    cpu_instr_SET(cpu, BIT_3, &cpu->BC.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xDA:  //SET 3, D
+                    cpu_instr_SET(cpu, BIT_3, &cpu->DE.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xDB:  //SET 3, E
+                    cpu_instr_SET(cpu, BIT_3, &cpu->DE.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xDC:  //SET 3, H
+                    cpu_instr_SET(cpu, BIT_3, &cpu->HL.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xDD:  //SET 3, L
+                    cpu_instr_SET(cpu, BIT_3, &cpu->HL.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xDE:  //SET 3, [HL]
+                    cpu_instr_SETHL(cpu, BIT_3, cpu->HL.r16);
+                    cpu->cycle = 16;
                     break;
         case 0xDF:  //SET 3, A
+                    cpu_instr_SET(cpu, BIT_3, &cpu->AF.r8.hi);
+                    cpu->cycle = 8;
                     break;
 
         case 0xE0:  //SET 4, B
+                    cpu_instr_SET(cpu, BIT_4, &cpu->BC.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xE1:  //SET 4, C
+                    cpu_instr_SET(cpu, BIT_4, &cpu->BC.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xE2:  //SET 4, D
+                    cpu_instr_SET(cpu, BIT_4, &cpu->DE.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xE3:  //SET 4, E
+                    cpu_instr_SET(cpu, BIT_4, &cpu->DE.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xE4:  //SET 4, H
+                    cpu_instr_SET(cpu, BIT_4, &cpu->HL.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xE5:  //SET 4, L
+                    cpu_instr_SET(cpu, BIT_4, &cpu->HL.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xE6:  //SET 4, [HL]
+                    cpu_instr_SETHL(cpu, BIT_4, cpu->HL.r16);
+                    cpu->cycle = 16;
                     break;
         case 0xE7:  //SET 4, A
+                    cpu_instr_SET(cpu, BIT_4, &cpu->AF.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xE8:  //SET 5, B
+                    cpu_instr_SET(cpu, BIT_5, &cpu->BC.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xE9:  //SET 5, C
+                    cpu_instr_SET(cpu, BIT_5, &cpu->BC.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xEA:  //SET 5, D
+                    cpu_instr_SET(cpu, BIT_5, &cpu->DE.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xEB:  //SET 5, E
+                    cpu_instr_SET(cpu, BIT_5, &cpu->DE.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xEC:  //SET 5, H
+                    cpu_instr_SET(cpu, BIT_5, &cpu->HL.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xED:  //SET 5, L
+                    cpu_instr_SET(cpu, BIT_5, &cpu->HL.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xEE:  //SET 5, [HL]
+                    cpu_instr_SETHL(cpu, BIT_5, cpu->HL.r16);
+                    cpu->cycle = 16;
                     break;
         case 0xEF:  //SET 5, A
+                    cpu_instr_SET(cpu, BIT_5, &cpu->AF.r8.hi);
+                    cpu->cycle = 8;
                     break;
 
         case 0xF0:  //SET 6, B
+                    cpu_instr_SET(cpu, BIT_6, &cpu->BC.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xF1:  //SET 6, C
+                    cpu_instr_SET(cpu, BIT_6, &cpu->BC.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xF2:  //SET 6, D
+                    cpu_instr_SET(cpu, BIT_6, &cpu->DE.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xF3:  //SET 6, E
+                    cpu_instr_SET(cpu, BIT_6, &cpu->DE.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xF4:  //SET 6, H
+                    cpu_instr_SET(cpu, BIT_6, &cpu->HL.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xF5:  //SET 6, L
+                    cpu_instr_SET(cpu, BIT_6, &cpu->HL.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xF6:  //SET 6, [HL]
+                    cpu_instr_SETHL(cpu, BIT_6, cpu->HL.r16);
+                    cpu->cycle = 16;
                     break;
         case 0xF7:  //SET 6, A
+                    cpu_instr_SET(cpu, BIT_6, &cpu->AF.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xF8:  //SET 7, B
+                    cpu_instr_SET(cpu, BIT_7, &cpu->BC.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xF9:  //SET 7, C
+                    cpu_instr_SET(cpu, BIT_7, &cpu->BC.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xFA:  //SET 7, D
+                    cpu_instr_SET(cpu, BIT_7, &cpu->DE.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xFB:  //SET 7, E
+                    cpu_instr_SET(cpu, BIT_7, &cpu->DE.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xFC:  //SET 7, H
+                    cpu_instr_SET(cpu, BIT_7, &cpu->HL.r8.hi);
+                    cpu->cycle = 8;
                     break;
         case 0xFD:  //SET 7, L
+                    cpu_instr_SET(cpu, BIT_7, &cpu->HL.r8.lo);
+                    cpu->cycle = 8;
                     break;
         case 0xFE:  //SET 7, [HL]
+                    cpu_instr_SETHL(cpu, BIT_7, cpu->HL.r16);
+                    cpu->cycle = 16;
                     break;
         case 0xFF:  //SET 7, A
+                    cpu_instr_SET(cpu, BIT_7, &cpu->AF.r8.hi);
+                    cpu->cycle = 8;
                     break;
 
         default: fprintf(stderr, "[ERROR] : ILLEGAL INSTRUCTION CB");
--- src/memory.c
+++ src/memory.c
@@ -1,4 +1,5 @@
 #include "memory.h"
+#include "hard_registers.h"
 #include <stdio.h>
 #include <string.h>
 
@@ -10,6 +11,35 @@ void memory_init(Memory* memory)
     }
 
     memset(memory->buffer, 0, sizeof(uint8_t) * MEMORY_SIZE);
+
+    memory->buffer[P1] = 0xCF;
+    memory->buffer[SC] = 0x7E;
+    memory->buffer[DIV] = 0xAB;
+    memory->buffer[TAC] = 0xF8;
+    memory->buffer[IF]  = 0xE1;
+    memory->buffer[NR10] = 0x80;
+    memory->buffer[NR11] = 0xBF;
+    memory->buffer[NR12] = 0xF3;
+    memory->buffer[NR13] = 0xFF;
+    memory->buffer[NR14] = 0xBF;
+    memory->buffer[NR21] = 0x3F;
+    memory->buffer[NR23] = 0xFF;
+    memory->buffer[NR24] = 0xBF;
+    memory->buffer[NR30] = 0x7F;
+    memory->buffer[NR31] = 0xFF;
+    memory->buffer[NR32] = 0x9F;
+    memory->buffer[NR33] = 0xFF;
+    memory->buffer[NR34] = 0xBF;
+    memory->buffer[NR41] = 0xFF;
+    memory->buffer[NR44] = 0xBF;
+    memory->buffer[NR50] = 0x77;
+    memory->buffer[NR51] = 0xF3;
+    memory->buffer[NR52] = 0xF1;
+    memory->buffer[LCDC] = 0x91;
+    memory->buffer[STAT] = 0x85;
+    memory->buffer[DMA] = 0xFF;
+    memory->buffer[BGP] = 0xFC;
+    
 }
 
 uint8_t memory_read8(Memory* memory, uint16_t address)
